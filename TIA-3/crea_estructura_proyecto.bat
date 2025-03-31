059cca1adfa17a37e1ed8d0cb01df4a912e26627
MagicExist <johhanparra1@gmail.com>
2025-03-30 22:13:30 -0500

    TIA-3-fix: Fix Project Structure in Bat File

--- TIA-3/crea_estructura_proyecto.bat
+++ TIA-3/crea_estructura_proyecto.bat
@@ -12,11 +12,11 @@ mkdir proyectos_pia-api\client\js
 
 :: Crear archivos
 type nul > proyectos_pia-api\server\config\database.js
-type nul > proyectos_pia-api\server\models\Usuario.js
-type nul > proyectos_pia-api\server\repositories\usuarios.js
-type nul > proyectos_pia-api\server\routes\usuarios.js
+type nul > proyectos_pia-api\server\models\tipo_proyecto.js
+type nul > proyectos_pia-api\server\repositories\tipo_proyecto.js
+type nul > proyectos_pia-api\server\routes\tipo_proyecto.js
 type nul > proyectos_pia-api\server\server.js
-type nul > proyectos_pia-api\server\migrations\001-create-usuarios.sql
+type nul > proyectos_pia-api\server\migrations\001-create-tipo-proyectos.sql
 type nul > proyectos_pia-api\client\index.html
 type nul > proyectos_pia-api\client\css\style.css
 type nul > proyectos_pia-api\client\js\app.js
