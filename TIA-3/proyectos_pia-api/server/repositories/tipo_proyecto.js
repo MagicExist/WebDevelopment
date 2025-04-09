51e0181fd1e5158179cf1fda6874fc8401e582c6
MagicExist <johhanparra1@gmail.com>
2025-04-09 12:29:39 -0500

    TIA-3-feature: add TipoProyectoRepository with getAll async method

---
+++ TIA-3/proyectos_pia-api/server/repositories/tipo_proyecto.js
@@ -0,0 +1,9 @@
+const db = require('../config/database')
+
+class TipoProyectoRepository {
+    async getAll() {
+        let query = "SELECT * FROM tipo_proyecto"
+        let tipo_proyectos = await db.executeQuery(query,[])
+        return tipo_proyectos
+    }
+}
