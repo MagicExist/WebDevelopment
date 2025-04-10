013f5e84e85115e852f78184c142c0c7a409c975
MagicExist <johhanparra1@gmail.com>
2025-04-10 18:23:00 -0500

    TIA-3-feature: add TipoProyectoRepository getById async method

--- TIA-3/proyectos_pia-api/server/repositories/tipo_proyecto.js
+++ TIA-3/proyectos_pia-api/server/repositories/tipo_proyecto.js
@@ -6,6 +6,11 @@ class TipoProyectoRepository {
         let tipo_proyectos = await db.executeQuery(query,[])
         return tipo_proyectos
     }
+    async getById(id){
+        const query = "SELECT * FROM tipo_proyecto WHERE id = $1"
+        let tipo_proyecto = await db.executeQuery(query,[id])
+        return tipo_proyecto
+    }
 }
 
 module.exports = new TipoProyectoRepository()
