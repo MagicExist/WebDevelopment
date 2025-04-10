cc6511f7bbb320709efa4800791cd0c0484b4d32
MagicExist <johhanparra1@gmail.com>
2025-04-10 18:47:04 -0500

    TIA-3-feature: add route get by id endpoint

--- TIA-3/proyectos_pia-api/server/routes/tipo_proyecto.js
+++ TIA-3/proyectos_pia-api/server/routes/tipo_proyecto.js
@@ -11,6 +11,21 @@ router.get('/',async (req,res)=>{
         res.status(500).json({'messsage' : "Error al obtener datos","Error" : error.message})
     }
 })
+
+router.get('/:id',async(req,res)=>{
+    try{
+        const {id} = req.params
+        if(isNaN(id)){
+            res.status(400).json({ "error": 'El ID debe ser un número' });
+        }
+
+        const result = await TipoProyectoRepository.getById(id)
+
+        if (result.length === 0) {
+            return res.status(404).json({ "error": 'Elemento no encontrado' });
+          }
+        
+        res.status(200).json(result)
     }
     catch (error){
         res.status(500).json({'messsage' : "Error al obtener datos","Error" : error.message})
