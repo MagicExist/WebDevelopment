a37753bca58fb8dd361893484efc916a7be322c5
MagicExist <johhanparra1@gmail.com>
2025-04-10 18:23:39 -0500

    TIA-3-fix: fix getAll async method, now query variable is a constant

--- TIA-3/proyectos_pia-api/server/repositories/tipo_proyecto.js
+++ TIA-3/proyectos_pia-api/server/repositories/tipo_proyecto.js
@@ -2,7 +2,7 @@ const db = require('../config/database')
 
 class TipoProyectoRepository {
     async getAll() {
-        let query = "SELECT * FROM tipo_proyecto"
+        const query = "SELECT * FROM tipo_proyecto"
         let tipo_proyectos = await db.executeQuery(query,[])
         return tipo_proyectos
     }
