bd4d91fb0548a5f85f7eae6735d114f321f78973
MagicExist <johhanparra1@gmail.com>
2025-04-10 20:18:14 -0500

    TIA-3-feature: add create tipo proyecto endpoint

--- TIA-3/proyectos_pia-api/server/routes/tipo_proyecto.js
+++ TIA-3/proyectos_pia-api/server/routes/tipo_proyecto.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const TipoProyectoRepository = require('../repositories/tipo_proyecto')
+const TipoProyectoMiddleware = require('../middleware/tipo_proyecto')
 
 router.get('/',async (req,res)=>{
     try{
@@ -23,9 +24,21 @@ router.get('/:id',async(req,res)=>{
 
         if (result.length === 0) {
             return res.status(404).json({ "error": 'Elemento no encontrado' });
-          }
+        }
         
         res.status(200).json(result)
+    }
+    catch (error){
+        res.status(500).json({'messsage' : "Error al obtener datos","Error" : error.message})
+    }
+})
+
+router.post("/",TipoProyectoMiddleware.validateData,async(req,res)=>{
+    try{
+        const data = req.body
+        const result = await TipoProyectoRepository.create(data)
+
+        res.status(201).json(result)
     }
     catch (error){
         res.status(500).json({'messsage' : "Error al obtener datos","Error" : error.message})
