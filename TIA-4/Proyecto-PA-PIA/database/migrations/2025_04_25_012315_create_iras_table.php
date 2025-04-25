b190c9ebf932f0dac36efb8b57bf6c1fe8e2f3dd
MagicExist <johhanparra1@gmail.com>
2025-04-24 20:31:00 -0500

    TIA-4-feature: add Ira  migration and model

---
+++ TIA-4/Proyecto-PA-PIA/database/migrations/2025_04_25_012315_create_iras_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('iras', function (Blueprint $table) {
+            $table->id();
+            $table->float('indice',2);
+            $table->foreignId('proyectos_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('iras');
+    }
+};
