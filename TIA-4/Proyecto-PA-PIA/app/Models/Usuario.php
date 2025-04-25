4e16e40ba300816f6a12b9eb6f71e30975be43ae
MagicExist <johhanparra1@gmail.com>
2025-04-24 20:49:04 -0500

    TIA-4-feature: add rol_usuario pivot table migration

--- TIA-4/Proyecto-PA-PIA/app/Models/Usuario.php
+++ TIA-4/Proyecto-PA-PIA/app/Models/Usuario.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Usuario extends Model
 {
-    //
+    public function rols(){
+        return $this->belongsToMany(Rol::class)->withTimestamps();
+    }
 }
